100f92bf04a2d4217dd0d011bb67b55b77924faa
Textterrion <terry994566@gmail.com>
2025-08-29 02:04:33 +0300

    start project 3000 v1.0.0

---
+++ vite.config.js
@@ -0,0 +1,57 @@
+import { defineConfig } from 'vite';
+import { globSync } from 'glob';
+import path from 'path';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import SortCss from 'postcss-sort-media-queries';
+
+export default defineConfig(({ command }) => {
+  const htmlFiles = globSync('./src/*.html');
+
+  const input = htmlFiles.reduce((acc, file) => {
+    const name = path.basename(file, path.extname(file));
+    acc[name] = path.resolve(file);
+    return acc;
+  }, {});
+
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
+    build: {
+      sourcemap: true,
+      outDir: '../dist',
+      emptyOutDir: true,
+      rollupOptions: {
+        input,
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
+        },
+      },
+    },
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/*.html']),
+      SortCss({
+        sort: 'mobile-first',
+      }),
+    ],
+  };
+});
